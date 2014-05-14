e15d50e5a1a3351334e583d6b07df1de806461ea
LKWI <lukaszkwiecinski90@gmail.com>
2014-05-14 18:01:49 +0200

    dodanie kalkulatora

---
+++ kalk.py
@@ -0,0 +1,12 @@
+print(">>")
+a = input()
+dzialanie = input()
+b = input()
+if(dzialanie == '+'):
+    result = int(a) + int(b)
+elif(dzialanie == '-'):
+    result = int(a) - int(b)
+else:
+    result = "BŁĄÐ"
+print("=")
+print(result)
